66f2c99d050622fa5f9bf73db71c050598a88311
jairosl <jairo.lima@aluno.uepb.edu.br>
2020-06-29 16:02:58 -0300

    Organizando o codigo

--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,20 @@
+// Update with your config settings.
+
 module.exports = {
-     development: {
-       client: 'pg',
-       connection: {
-         database: "knexsercomp",
-         user: "postgres",
-         password: "kaka2210"
-       },
-       migrations: {
-         tableName: 'knex_migrations',
-         directory: `${__dirname}/src/database/migrations`
-       },
-       seeds: {
-         directory: `${__dirname}/src/database/seeds`
-       }
-     },
-     onUpdateTrigger: table  => 
-     `CREATE TRIGGER ${table}_updated_at
-     BEFORE UPDATE ON ${table}
-     FOR EACH ROW
-     EXECUTE PROCEDURE on_update_timestamp();`
-     }
+  development: {
+    client: 'pg',
+    connection: {
+      host: "localhost",
+      user: "postgres",
+      password: "jairosl10",
+      atabase: "knexsercomp"
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  },
+  }
