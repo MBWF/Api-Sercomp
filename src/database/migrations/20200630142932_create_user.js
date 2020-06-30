215d25a351c2ac0a28516e6f15811afbfe7a54f4
jairosl <jairo.lima@aluno.uepb.edu.br>
2020-06-30 15:09:23 -0300

    Criando a tabela de Usuario

--- src/database/migrations/20200630142932_create_user.js
+++ src/database/migrations/20200630142932_create_user.js
@@ -15,6 +15,7 @@ exports.up = async knex => knex.schema.createTable('usuario', table => {
     .onDelete('SET NULL')
 
     table.timestamp('created_at', { precision: 6 }).defaultTo(knex.fn.now(6))
+    
 })
 
 exports.down = async knex => knex.schema.dropTable('usuario')
