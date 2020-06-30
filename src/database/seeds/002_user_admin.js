9404fbe0597e4e1ef169abbc8069476f36d7c5c8
jairosl <jairo.lima@aluno.uepb.edu.br>
2020-06-30 15:30:29 -0300

    Seed criado e usuario admin cadastrado

---
+++ src/database/seeds/002_user_admin.js
@@ -0,0 +1,17 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('usuario').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('usuario').insert([
+        {
+          id_perfil: 13,
+          email: 'admin@sercomp.com',
+          senha: '$2a$08$CAUky5gNGCENQn3yhH3.9.ohwj3B72gRO/fa7e5jbopSWXOvZsWgK', //sercomp
+          name: 'Sercomp Admin',
+        },
+        
+      ]);
+    });
+};
